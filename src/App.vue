dec735340b8cbceae22131201df39f86880cce96
Konstantin Babushkin <kostya-babushkin@yandex.ru>
2023-11-11 13:37:08 +0100

    Vue CLI init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <img alt="Vue logo" src="./assets/images/logo.png" />
+    <MainPage msg="Выпускной проект из Яндекс Практикума" />
+  </div>
+</template>
+
+<script>
+import MainPage from "./components/MainPage.vue";
+
+export default {
+  name: "App",
+  components: {
+    MainPage,
+  },
+};
+</script>
+
+<!-- <style>
+</style> -->
